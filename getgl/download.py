6f458586fb7cefe4974b6a39285b2e0947cadb5e
Krai53n <greasha46@gmail.com>
2024-04-21 03:02:28 +0300

    add getting funcs urls

--- getgl/download.py
+++ getgl/download.py
@@ -11,6 +11,14 @@ from urllib import request
 from lxml import etree
 
 
+GLSL_BASE_URL = 'https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/'
+GL_BASE_URL = 'https://registry.khronos.org/OpenGL-Refpages/gl4/html/'
+
+
+def get_url(base: str, what: str, ext: str):
+    return f'{base}{what}.{ext}'
+
+
 def get_funcs(url: str, find_str: str, skip: Iterable[str]) -> list[str]:
     response = request.urlopen(url)
     html = bytes().join(response.readlines())
@@ -18,16 +26,28 @@ def get_funcs(url: str, find_str: str, skip: Iterable[str]) -> list[str]:
     return [i.text for i in root.iterfind(find_str) if i.text not in skip]
 
 
+def download(what: str):
+    match what:
+        case 'glsl':
+            pass
+        case 'gl':
+            pass
+    raise Exception("`what` waiting 'glsl' or 'gl' string")
+
+
 if __name__ == '__main__':
+    from pprint import pprint
     glsl_funcs = get_funcs(
-        url='https://registry.khronos.org/OpenGL-Refpages/es2.0/xhtml/index.html',
+        url=get_url(GLSL_BASE_URL, 'index', 'html'),
         find_str='.//a[@target]',
         skip=tuple(),
     )
     gl_funcs = get_funcs(
-        url='https://registry.khronos.org/OpenGL-Refpages/gl4/html/indexflat.php',
+        url=get_url(GL_BASE_URL, 'indexflat', 'php'),
         find_str='.//a[@target]',
         skip=('Introduction', ),
     )
 
-    print(gl_funcs)
+    pprint(glsl_funcs)
+    pprint(gl_funcs)
+    # download('glsl')
